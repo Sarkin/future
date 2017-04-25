b1bc2ed3d9bdc2bd51eae5c8016e1957e175fc83
Alsarmini Kinan Mamunovich <kinan.sarmini@gmail.com>
2017-04-25 23:04:59 +0300

    future/promise + async

--- future.h
+++ future.h
@@ -1,23 +1,42 @@
 #pragma once
 
+#include <condition_variable>
 #include <exception>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
+#include <thread>
+#include <type_traits>
+#include <vector>
 
-template<typename T>
+namespace kinan {
+
+template <typename T>
 class shared_state {
 public:
-    void set_value(T&& v);
-    void set_value(const T& v);
+    shared_state() : is_initialized_(false) { }
+    template <typename U>
+    void set_value(U&& v);
     void set_exception(std::exception_ptr e);
 
     T get();
     T try_get();
+    void wait();
+
+private:
+    bool is_initialized_;
+    std::exception_ptr error_;
+    std::unique_ptr<T> state_;
+    mutable std::mutex init_mutex_;
+    std::condition_variable init_cv_;
 };
 
-template<typename T>
+template <typename T>
 class future {
 public:
-    future();
-    ~future();
+    future(std::shared_ptr<shared_state<T>> state) : state_(state) { }
+    ~future() { }
     future(const future& rhs) = delete;
     future(future&&) = default;
     future& operator=(const future& rhs) = delete;
@@ -25,13 +44,17 @@ public:
 
     T get();
     T try_get();
+    void wait();
+
+private:
+    std::shared_ptr<shared_state<T>> state_;
 };
 
-template<typename T>
+template <typename T>
 class promise {
 public:
-    promise();
-    ~promise();
+    promise() : state_(std::make_shared<shared_state<T>>()) { }
+    ~promise() { }
     promise(const promise& rhs) = delete;
     promise(promise&&) = default;
     promise& operator=(const promise& rhs) = delete;
@@ -42,9 +65,132 @@ public:
     void set_exception(std::exception_ptr e);
 
     future<T> get_future();
+
+    /*
+    template <typename U>
+    future<T> then(future<U> f);
+    */
+
+private:
+    std::shared_ptr<shared_state<T>> state_;
 };
 
-template<typename T>
-promise::promise() {
+class thread_pool {
+public:
+    thread_pool(size_t);
+    template <typename F, typename... Args, bool async>
+    void run(F&& f, Args&&... args);
+    ~thread_pool() = default;
 
+private:
+    std::vector<std::thread> workers_;
+    std::mutex queue_mutex_;
+};
+
+template <bool try_async = false, typename F, typename... Args>
+auto async(F&& f, Args&&... args) -> future<std::result_of<F(Args...)>::type> {
+    using return_type = std::result_of<F(Args...)>::type;
+    auto p = promise<return_type>();
+    p.set_value(f(args));
+    return p.get_feature();
 }
+
+
+template <bool try_async = true, typename F, typename... Args>
+auto async(F&& f, Args&&... args) -> future<std::result_of<F(Args...)>::type> {
+    using return_type = std::result_of<F(Args...)>::type;
+    std::shared_ptr<promise<return_type>> p;
+    tp.run([=] {
+        try {
+            p->set_value(f(args...));
+        } catch (...) {
+            p->set_exception(std::current_exception());
+        }
+    });
+    return p->get_feature();
+}
+
+
+template <typename T>
+template <typename U>
+void shared_state<T>::set_value(U&& v) {
+    std::lock_guard<std::mutex> lg(init_mutex_);
+    state_ = std::make_unique<T>(std::forward<U>(v));
+    is_initialized_ = true;
+    init_cv_.notify_all();
+}
+
+template <typename T>
+void shared_state<T>::set_exception(std::exception_ptr e) {
+    std::lock_guard<std::mutex> lg(init_mutex_);
+    error_ = e;
+    is_initialized_ = true;
+    init_cv_.notify_all();
+}
+
+template <typename T>
+void shared_state<T>::wait() {
+    std::unique_lock<std::mutex> lk(init_mutex_);
+    init_cv_.wait(lk, [this]{return is_initialized_;});
+}
+
+template <typename T>
+T shared_state<T>::get() {
+    wait();
+    return try_get();
+}
+
+template <typename T>
+T shared_state<T>::try_get() {
+    if (error_) {
+        std::rethrow_exception(error_);
+    }
+    if (state_) {
+        return std::move(*state_);
+    }
+    throw std::logic_error("invalid state");
+}
+
+template <typename T>
+void promise<T>::set_value(T&& v) {
+    state_->set_value(std::move(v));
+}
+
+template <typename T>
+void promise<T>::set_value(const T& v) {
+    state_->set_value(v);
+}
+
+template <typename T>
+void promise<T>::set_exception(std::exception_ptr e) {
+    state_->set_exception(e);
+}
+
+templatlambdae <typename T>
+future<T> promise<T>::get_future() {
+    return future<T>(state_);
+}
+
+template <typename T>
+T future<T>::get() {
+    return state_->get();
+}
+
+template <typename T>
+T future<T>::try_get() {
+    return state_->try_get();
+}
+
+template <typename T>
+void future<T>::wait() {
+    return state_->wait();
+}
+
+/*
+template <typename T>
+template <typename U>
+future<T> future<T>::then(future<U> f) {
+}
+*/
+
+} // namespace kinan
